cfbe3993765dac813aafb01716bbd9cbf84f6a5a
mukumuku <andreletourneau2@gmail.com>
2025-05-13 01:07:59 -0500

    steady steady debugging

--- ASM/SteadySteady/SteadySteady.c
+++ ASM/SteadySteady/SteadySteady.c
@@ -6,12 +6,16 @@ extern int SkillTester(struct BattleUnit* unit, int id);
 extern int prMovGetter(struct Unit* unit); 
 extern int SteadySteadyID_Link;
 
-void SteadySteadyEffect(struct BattleUnit* bunitA, struct BattleUnit* bunitB) {	
-	if (SkillTester(&gBattleActor, SteadySteadyID_Link) == true) {
-		if (gActionData.moveCount <= (prMovGetter(&gBattleActor.unit) - 1)) {
-			gBattleActor.battleCritRate += 15;
-			gBattleActor.battleHitRate += 30;
-			gBattleActor.battleAttack += (GetItemMight(&gBattleActor.weapon) / 4);
+void SteadySteadyEffect(struct BattleUnit* bunitA, struct BattleUnit* bunitB) {
+	if(bunitA == &gBattleActor) {
+		if(SkillTester(bunitA, SteadySteadyID_Link)) {
+			gEventSlots[8] = (prMovGetter(&gBattleActor.unit)/2);
+			gEventSlots[9] = gActionData.moveCount;
+			if (gActionData.moveCount <= ((prMovGetter(&gBattleActor.unit)/2) - 1)) {
+				gBattleActor.battleCritRate += 15;
+				gBattleActor.battleHitRate += 30;
+				gBattleActor.battleAttack += (GetItemMight(&gBattleActor.weapon) / 4);
+			}
 		}
 	}
 	return;
