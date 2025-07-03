a2ce891b74591f3391608ae003bb7136eee4246a
mukumuku <andreletourneau2@gmail.com>
2025-07-03 15:45:24 -0500

    Update AnvilDrop.c

--- ASM/AnvilDrop/AnvilDrop.c
+++ ASM/AnvilDrop/AnvilDrop.c
@@ -8,7 +8,7 @@ extern int AnvilDropID_Link;
 void AnvilDropEffect(struct BattleUnit* bunitA, struct BattleUnit* bunitB) {
 	if(SkillTester(bunitB, AnvilDropID_Link) == true) { //checking unit B because if I check unit A, it won't have their AS because it isn't populated yet (iirc?)
 		if (!((bunitB->battleSpeed - bunitA->battleSpeed) > BATTLE_FOLLOWUP_SPEED_THRESHOLD)) { //if can't double
-			bunitB->battleAttack += (GetItemMight(bunitB->weapon)/2); //double weapon mt
+			bunitB->battleAttack += (GetItemMight(bunitB->weapon)/2); //+1/2 weapon mt
 		}
 	}
 	return;
