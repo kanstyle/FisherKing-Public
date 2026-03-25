6d85294359528ccde9db749419ff2ec71785eac5
kanstyle <>
2026-03-25 18:33:24 -0500

    item icons and more writing

--- ASM/Shikari/Shikari.c
+++ ASM/Shikari/Shikari.c
@@ -6,18 +6,20 @@ extern int SkillTester(struct Unit* unit, int id);
 extern int ShikariID_Link;
 
 void ShikariEffect(struct BattleUnit* bunitA, struct BattleUnit* bunitB) {
-	int enemyWep = bunitA->weaponType;
-	if (SkillTester(bunitB, ShikariID_Link) == 0) {
-		//gEventSlots[6] = 0x5;
-		return;
-	} else {
-		//gEventSlots[6] = 0x4;
-	}
-	if (enemyWep == 0xB) { //monster weapon
-		gEventSlots[6] = enemyWep;
-		bunitB->wTriangleHitBonus = 20;
-		bunitB->wTriangleDmgBonus = 2;
-		bunitA->wTriangleHitBonus = -20;
-		bunitA->wTriangleDmgBonus = -2;
-	}
+    if (SkillTester(bunitA, ShikariID_Link) != 0) {
+        if (bunitB->weaponType == 0xB) { //monster weapon
+            bunitA->wTriangleHitBonus = 20;
+            bunitA->wTriangleDmgBonus = 2;
+            bunitB->wTriangleHitBonus = -20;
+            bunitB->wTriangleDmgBonus = -2;
+        }
+    }
+    if (SkillTester(bunitB, ShikariID_Link) != 0) {
+        if (bunitA->weaponType == 0xB) { //monster weapon
+            bunitB->wTriangleHitBonus = 20;
+            bunitB->wTriangleDmgBonus = 2;
+            bunitA->wTriangleHitBonus = -20;
+            bunitA->wTriangleDmgBonus = -2;
+        }
+    }
 }
